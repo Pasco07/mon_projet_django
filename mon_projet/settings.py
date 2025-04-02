96c93cdc959cfc7a6719e96f657d6f9407ba9be4
tchatti <tchattipascal99@gmail.com>
2025-04-02 18:29:29 +0100

    redeploiement1

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = config ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://application-django.onrender.com"]
+ALLOWED_HOSTS = [
+    '127.0.0.1', 
+    'localhost',
+    'application-django.onrender.com',  # Votre domaine Render
+    '.onrender.com'  # Permet tous les sous-domaines Render
+]
 
 
 # Application definition
@@ -98,18 +103,29 @@ WSGI_APPLICATION = 'mon_projet.wsgi.application'
 # }
 
 # Configuration DEV (MySQL local)
-# settings.py
-import os
-import dj_database_url
-from decouple import config
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', 'postgresql://mtp_user:password@dpg-cvlt3n6mcj7s73e9muug-a.oregon-postgres.render.com/mtp'),
-        conn_max_age=600,
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mtp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    }
+}
+
+# Surcharge PROD (PostgreSQL sur Render)
+if 'RENDER' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        default=config('DATABASE_URL'),  # Ajout important
         ssl_require=True
     )
-}
+    # Forcer le moteur PostgreSQL
+    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
